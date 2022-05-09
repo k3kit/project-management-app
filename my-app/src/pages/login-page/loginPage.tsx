9c6a89afe45f657dfa379745a610961ba4920833
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-09 17:15:48 +0300

    feat: add login page

--- my-app/src/pages/login-page/loginPage.tsx
+++ my-app/src/pages/login-page/loginPage.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { Copyright } from '@mui/icons-material';
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Grid,
+} from '@mui/material';
+import React, { useMemo } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  return <div>loginPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const navigatePathname = useMemo(() => {
+    const state = location.state as { from: string };
+
+    if (state && state.from) {
+      return state.from;
+    }
+
+    return '/';
+  }, [location]);
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs></Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default LoginPage;
