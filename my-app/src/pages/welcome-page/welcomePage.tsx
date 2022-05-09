40d5a8574fec4b4e55eba190e02b8f3ea3561ed8
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-09 17:16:32 +0300

    refactor: changed welcome page

--- my-app/src/pages/welcome-page/welcomePage.tsx
+++ my-app/src/pages/welcome-page/welcomePage.tsx
@@ -1,11 +1,30 @@
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
   return (
     <>
-      <div>WelcomePage</div>
-      <Link to="/login">Login / Sign up</Link>
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography sx={{ flexGrow: 1 }} variant="h6">
+              Project management app
+            </Typography>
+            <Button sx={{ mr: 2 }} variant="contained">
+              <Link to="/login">Sign In</Link>
+            </Button>
+            <Button sx={{ mr: 2 }} variant="contained">
+              <Link to="/register">Sign Up</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <Container>
+          <Typography>Welcome</Typography>
+        </Container>
+      </Box>
     </>
   );
 };
