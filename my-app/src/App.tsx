0b1cf5828483e7093d26ffe9f4e2e217d5a25a2c
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-09 17:17:09 +0300

    fix: fix route

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,32 +1,59 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import './index.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import WelcomePage from './pages/welcome-page/welcomePage';
 import MainPage from './pages/main-page/mainPage';
 import LoginPage from './pages/login-page/loginPage';
 import BoardPage from './pages/board-page/boardPage';
 import Layout from './components/Layout/Layout';
+import { RequireAuth } from './hoc/RequireAuth';
+import Register from './pages/login-page/Register';
 function App() {
-  // useEffect(() => {
-  //   fetch('https://still-dusk-31338.herokuapp.com/users')
-  //     .then((res) => res.json())
-  //     .then(
-  //       (data) => {
-  //         console.log(data);
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // }, []);
+  useEffect(() => {
+    const loginUser = async (user: { name: string; login: string; password: string }) => {
+      const rawResponse = await fetch('https://still-dusk-31338.herokuapp.com/signup', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      const content = await rawResponse.json();
+
+      console.log(content);
+    };
+
+    loginUser({
+      name: 'Vasyasdf',
+      login: 'user00ss1',
+      password: 'userpass@1sdf23',
+    });
+  }, []);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<WelcomePage />} />
           <Route path="login" element={<LoginPage />} />
-          <Route path="main" element={<MainPage />} />
-          <Route path="board" element={<BoardPage />} />
+          <Route path="register" element={<Register />} />
+          <Route
+            path="main"
+            element={
+              <RequireAuth>
+                <MainPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="board"
+            element={
+              <RequireAuth>
+                <BoardPage />
+              </RequireAuth>
+            }
+          />
         </Route>
       </Routes>
     </div>
