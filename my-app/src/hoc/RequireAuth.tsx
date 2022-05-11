4c2b924ac0518ca7d3ba6a10851829c55570f32f
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-11 16:21:20 +0300

    fix: changed check

--- my-app/src/hoc/RequireAuth.tsx
+++ my-app/src/hoc/RequireAuth.tsx
@@ -1,10 +1,13 @@
 import { useLocation, Navigate } from 'react-router-dom';
 import React from 'react';
 import WelcomePage from '../pages/welcome-page/welcomePage';
-export const auth = false;
-export const RequireAuth = ({ children }: { children: JSX.Element }) => {
-  const location = useLocation();
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
 
+export const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const dispath = useAppDispatch();
+  const { isLoggedIn } = useAppSelector((state) => state.authReducer);
+  const location = useLocation();
+  const auth = isLoggedIn;
   if (!auth) {
     return <Navigate to="/" state={{ from: location }} />;
   }
