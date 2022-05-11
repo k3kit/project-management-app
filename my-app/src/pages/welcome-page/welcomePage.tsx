ee019531a80ad6558826d92b65634fde6b571109
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-11 16:14:56 +0300

    fix: fix button login/register

--- my-app/src/pages/welcome-page/welcomePage.tsx
+++ my-app/src/pages/welcome-page/welcomePage.tsx
@@ -2,19 +2,13 @@ import { Login } from '@mui/icons-material';
 import { AppBar, Box, Button, Container, Dialog, Toolbar, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Modal from '../../components/modal';
+import { useAppSelector } from '../../hooks/redux';
 import LoginPage from '../login-page/loginPage';
 import Register from '../login-page/Register';
 const WelcomePage = () => {
   const [openSignIn, setOpenSignIn] = useState(false);
   const [openSignUp, setOpenSignUp] = useState(false);
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
+  const { isLoggedIn } = useAppSelector((state) => state.authReducer);
   return (
     <>
       <Box>
@@ -23,24 +17,33 @@ const WelcomePage = () => {
             <Typography sx={{ flexGrow: 1 }} variant="h6">
               Project management app
             </Typography>
-            <Button sx={{ mr: 2 }} variant="contained" onClick={() => setOpenSignIn(true)}>
-              Sign In
-            </Button>
-            <Button sx={{ mr: 2 }} variant="contained" onClick={() => setOpenSignUp(true)}>
-              Sign Up
-            </Button>
+            {isLoggedIn ? (
+              <Button sx={{ mr: 2 }} variant="contained">
+                <Link to="main"> Go to Main Page</Link>
+              </Button>
+            ) : (
+              <>
+                <Button sx={{ mr: 2 }} variant="contained" onClick={() => setOpenSignIn(true)}>
+                  Sign In
+                </Button>
+                <Button sx={{ mr: 2 }} variant="contained" onClick={() => setOpenSignUp(true)}>
+                  Sign Up
+                </Button>
+              </>
+            )}
           </Toolbar>
         </AppBar>
       </Box>
       <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
         <Container>
           <Dialog open={openSignIn} onClose={() => setOpenSignIn(false)}>
-            <LoginPage />
+            <LoginPage setOpenSignIn={setOpenSignIn} />
           </Dialog>
           <Dialog open={openSignUp} onClose={() => setOpenSignUp(false)}>
-            <Register />
+            <Register setOpenSignUp={setOpenSignUp} />
           </Dialog>
           <Typography>Welcome</Typography>
+          <Link to="main">main</Link>
         </Container>
       </Box>
     </>
