0f2af1cbd90e86e22119d45f18d3b52f50983125
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-11 17:16:16 +0300

    feat: add animated sticky header

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, Button, Stack, Toolbar, Typography, Switch } from '@mui/material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
 
 export const Header = () => {
+  const [fix, setFix] = useState(false);
+  const setFixed = () => {
+    if (window.scrollY >= 100) {
+      setFix(true);
+    } else {
+      setFix(false);
+    }
+  };
+  window.addEventListener('scroll', setFixed);
   return (
-    <AppBar position="sticky">
+    <AppBar className={fix ? 'appBar fixed' : 'appBar'} position="sticky">
       <Toolbar>
         <Button color="inherit">edit profile</Button>
         <Button color="inherit">logout</Button>
