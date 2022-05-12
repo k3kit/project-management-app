6b8d8ad2fdfb132506147e41a351ef32b350b99b
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-12 14:25:39 +0300

    feat: add exit profile

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { AppBar, Button, Stack, Toolbar, Typography, Switch } from '@mui/material';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
-
+import { useAppDispatch } from '../hooks/redux';
+import { authSlice } from '../store/slices/Auth';
+import { logout } from '../store/slices/Auth';
 export const Header = () => {
   const [fix, setFix] = useState(false);
+  const dispatch = useAppDispatch();
   const setFixed = () => {
     if (window.scrollY >= 100) {
       setFix(true);
@@ -13,11 +16,16 @@ export const Header = () => {
     }
   };
   window.addEventListener('scroll', setFixed);
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return (
     <AppBar className={fix ? 'appBar fixed' : 'appBar'} position="sticky">
       <Toolbar>
         <Button color="inherit">edit profile</Button>
-        <Button color="inherit">logout</Button>
+        <Button color="inherit" onClick={handleLogout}>
+          logout
+        </Button>
         <Button color="inherit">create new board</Button>
         <Stack direction="row" spacing={1} alignItems="center">
           <Typography>EN</Typography>
