0673922606ff0ad1243df29e69ee5345fb64554f
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-13 16:24:47 +0300

    feat: add user service

--- my-app/src/services/user.service.ts
+++ my-app/src/services/user.service.ts
@@ -1,2 +1,27 @@
 import axios from 'axios';
+import authHeader from './auth-header';
 const API_URL = 'https://still-dusk-31338.herokuapp.com/';
+
+const getAllUsers = () => {
+  return axios.get(API_URL + 'users', { headers: authHeader() });
+};
+
+const getUser = (id: string) => {
+  return axios.get(API_URL + `users/${id}`, { headers: authHeader() });
+};
+
+const deleteUser = (id: string) => {
+  return axios.delete(API_URL + `users/${id}`, { headers: authHeader() });
+};
+const updateUser = (id: string) => {
+  return axios.delete(API_URL + `users/${id}`, { headers: authHeader() }).then((response) => {
+    return response.data;
+  });
+};
+const userService = {
+  getAllUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+};
+export default userService;
