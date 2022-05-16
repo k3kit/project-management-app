fbc02734bcf9ee2dec853fb1563e6337e1c7c6ff
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-16 13:21:13 +0300

    fix: fix url api

--- my-app/src/services/boards.service.ts
+++ my-app/src/services/boards.service.ts
@@ -1,27 +1,27 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const API_URL = 'https://still-dusk-31338.herokuapp.com/boards/';
+const API_URL = 'https://still-dusk-31338.herokuapp.com/';
 
 const getAllBoards = () => {
-  return axios.get(API_URL, { headers: authHeader() });
+  return axios.get(API_URL + 'boards', { headers: authHeader() });
 };
 
 const createBoard = (title: string) => {
-  return axios.post(API_URL, { title }, { headers: authHeader() });
+  return axios.post(API_URL + 'boards', { title }, { headers: authHeader() });
 };
 
 const boardById = (id: string) => {
-  return axios.get(API_URL + id, {
+  return axios.get(`https://still-dusk-31338.herokuapp.com/boards/${id}`, {
     headers: authHeader(),
   });
 };
 
 const deleteBoard = (id: string) => {
-  return axios.delete(API_URL + id, { headers: authHeader() });
+  return axios.delete(API_URL + 'boards/' + id), { headers: authHeader() };
 };
 
 const updateBoard = (id: string, title: string) => {
-  return axios.put(API_URL + id, { title }, { headers: authHeader() });
+  return axios.put(API_URL + 'boards/' + id), { title }, { headers: authHeader() };
 };
 const boardsService = {
   getAllBoards,
