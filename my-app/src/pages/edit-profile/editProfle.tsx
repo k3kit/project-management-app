07fd03b769fb389d0ff2ddd1e2fe1eb70150e41f
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-16 13:03:16 +0300

    refactor: change markup

--- my-app/src/pages/edit-profile/editProfle.tsx
+++ my-app/src/pages/edit-profile/editProfle.tsx
@@ -1,3 +1,4 @@
+import { Box, Container } from '@mui/material';
 import React from 'react';
 import EditProfile from '../../components/editProfile';
 import { Header } from '../../components/header';
@@ -6,7 +7,9 @@ const EditPage = () => {
   return (
     <>
       <Header />
-      <EditProfile />
+      <Box sx={{ height: '100vh' }}>
+        <EditProfile />
+      </Box>
     </>
   );
 };
