a2ca4c01a61b139a9b3757840c280bc1464d9d1a
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-16 13:02:10 +0300

    refactor: update user slice

--- my-app/src/store/slices/user.ts
+++ my-app/src/store/slices/user.ts
@@ -2,24 +2,22 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import authService from '../../services/auth.service';
 import userService from '../../services/user.service';
 interface userData {
-  name: string;
-  login: string;
-  password: string;
+  id: string;
 }
 export const getUsers = createAsyncThunk('/users', async (_, thunkAPI) => {
   try {
-    const data = await userService.getAllUsers();
-    console.log(data);
-    return data;
+    const response = await userService.getAllUsers();
+    console.log(response);
+    return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
   }
 });
-export const deleteUsers = createAsyncThunk('/users', async (_, thunkAPI) => {
+export const deleteUsers = createAsyncThunk('/users', async (id: string, thunkAPI) => {
   try {
-    const data = await userService.deleteUser('3a2380a3-dd37-4a5e-8f49-57821f262ee5');
-    console.log(data);
-    return data;
+    const response = await userService.deleteUser(id);
+    console.log(response.data);
+    return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
   }
