9edab00b0172dd60a454ef704df5e39b15dac484
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-16 13:05:21 +0300

    refactor: add confirm modal to edit edit profile and made exit from profile

--- my-app/src/components/editProfile.tsx
+++ my-app/src/components/editProfile.tsx
@@ -5,20 +5,20 @@ import {
   Typography,
   TextField,
   Button,
-  Snackbar,
   Alert,
   Grid,
 } from '@mui/material';
-import { error } from 'console';
-import React from 'react';
-import { Controller } from 'react-hook-form';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { ModalProfileSlice } from '../store/slices/header';
+import { deleteUsers } from '../store/slices/user';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import ConfirmDialog from './ConfirmationModal';
+import { logout } from '../store/slices/Auth';
 const validationShema = yup.object().shape({
   name: yup
     .string()
@@ -41,8 +41,14 @@ interface IFormEdit {
   login: string;
   password: string;
 }
+export interface Jwt {
+  iat?: number;
+  login?: string;
+  userId: string;
+}
 const EditProfile = () => {
   const dispath = useAppDispatch();
+  const [confirmOpen, setConfirmOpen] = useState(false);
   const { setOpenEditProfile } = ModalProfileSlice.actions;
   const { onedEditProfile } = useAppSelector((state) => state.modalReducer);
   const {
@@ -52,6 +58,17 @@ const EditProfile = () => {
   } = useForm<IFormEdit>({
     resolver: yupResolver(validationShema),
   });
+
+  const handleDelete = () => {
+    const tokenA = JSON.parse(localStorage.getItem('token') || 'null');
+    const tok = tokenA.token;
+    console.log(tok);
+
+    const decoded = jwtDecode<Jwt>(tok);
+    console.log(decoded.userId);
+    dispath(deleteUsers(decoded.userId));
+    dispath(logout());
+  };
   return (
     <Container maxWidth="xs">
       <CssBaseline />
@@ -114,6 +131,7 @@ const EditProfile = () => {
             disableElevation={true}
             variant="outlined"
             color="error"
+            onClick={() => setConfirmOpen(true)}
             startIcon={<DeleteIcon />}
             sx={{
               marginTop: 1,
@@ -122,6 +140,14 @@ const EditProfile = () => {
           >
             Delete profile
           </Button>
+          <ConfirmDialog
+            title="Delete profile?"
+            open={confirmOpen}
+            setOpen={setConfirmOpen}
+            onConfirm={handleDelete}
+          >
+            Are you sure you want to delete this profile?
+          </ConfirmDialog>
           <Button
             type="submit"
             variant="contained"
