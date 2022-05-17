63be61a3793ffc39600305ec9ef00cd2e60756d1
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-17 13:12:09 +0300

    refactor: change main page

--- my-app/src/pages/main-page/mainPage.tsx
+++ my-app/src/pages/main-page/mainPage.tsx
@@ -1,32 +1,34 @@
-import { Container, Dialog, Grid, Typography } from '@mui/material';
+import { Container, Dialog, Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect } from 'react';
+import { BoardModal } from '../../components/BoardModal';
 import { Board } from '../../components/boards';
-// import EditProfile from '../../components/editProfile';
 import { Header } from '../../components/header';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import { getBoards } from '../../store/slices/boards';
-// import { ModalProfileSlice } from '../../store/slices/header';
+import { boardsSlice, getBoards } from '../../store/slices/boards';
 
 const MainPage = () => {
   const dispath = useAppDispatch();
-  // const { setOpenEditProfile } = ModalProfileSlice.actions;
-  // const { onedEditProfile } = useAppSelector((state) => state.modalReducer);
-  const { boards } = useAppSelector((state) => state.boardReducer);
+  const { setOpen } = boardsSlice.actions;
+  const { Modal, boards } = useAppSelector((state) => state.boardReducer);
+
   useEffect(() => {
     dispath(getBoards());
   }, [dispath]);
 
   return (
     <>
-      <Header />{' '}
+      <Header />
       <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', marginTop: 2 }}>
         <Container id="main">
           <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 12 }}>
             {boards.map(({ id, title }) => (
-              <Board key={id} id={id} title={title} />
+              <Board key={id} id={id} title={title} setConfirmOpen={Modal} />
             ))}
           </Grid>
+          <Dialog open={Modal} onClose={() => dispath(setOpen(false))}>
+            <BoardModal />
+          </Dialog>
         </Container>
       </Box>
     </>
