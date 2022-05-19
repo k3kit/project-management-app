86a6aac3b455307ad43aec84d0df1c60f7bec13c
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-19 17:33:16 +0300

    fix: fix columns service

--- my-app/src/services/columns.service.ts
+++ my-app/src/services/columns.service.ts
@@ -3,8 +3,8 @@ import { StringMappingType } from 'typescript';
 import authHeader from './auth-header';
 const API_URL = 'https://still-dusk-31338.herokuapp.com/boards/';
 
-const createColumns = (title: string, boardId: string) => {
-  return axios.post(`${API_URL}${boardId}/columns`, { title }, { headers: authHeader() });
+const createColumns = (boardId: string, column: IColum) => {
+  return axios.post(`${API_URL}${boardId}/columns`, column, { headers: authHeader() });
 };
 
 const getAllColumns = (boardId: string) => {
