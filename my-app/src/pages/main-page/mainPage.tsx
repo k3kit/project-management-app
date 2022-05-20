4f2358beb49aeeb8898eb67acf4070b3ade089fe
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-20 16:18:58 +0300

    refactor:  add error and loading indicator

--- my-app/src/pages/main-page/mainPage.tsx
+++ my-app/src/pages/main-page/mainPage.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
-import { Container, Dialog, Grid } from '@mui/material';
+import { Container, Dialog, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { BoardModal } from '../../components/BoardModal';
 import { Board } from '../../components/Boards';
 import { Header } from '../../components/header';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { boardsSlice, getBoards } from '../../store/slices/boards';
+import { Loading } from '../../components/Loading';
+import { Error } from '../../components/Error';
 
 const MainPage = () => {
   const dispath = useAppDispatch();
   const { setOpen } = boardsSlice.actions;
-  const { Modal, boards } = useAppSelector((state) => state.boardReducer);
+  const { Modal, boards, isLoading, error } = useAppSelector((state) => state.boardReducer);
 
   useEffect(() => {
     dispath(getBoards());
@@ -21,6 +23,8 @@ const MainPage = () => {
       <Header />
       <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', marginTop: 2 }}>
         <Container id="main">
+          {isLoading && <Loading />}
+          {error && <Error message={error} />}
           <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 12 }}>
             {boards.map(({ id, title }) => (
               <Board key={id} id={id} title={title} setConfirmOpen={Modal} />
