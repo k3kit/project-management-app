89a459a2f01c991bb9d3c1bf44a2c25643268833
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-22 16:31:01 +0300

    refactor: add  сonfirmation modal to page with columns

--- my-app/src/pages/board-page/boardItem.tsx
+++ my-app/src/pages/board-page/boardItem.tsx
@@ -16,10 +16,12 @@ import CheckIcon from '@mui/icons-material/Check';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import ClearIcon from '@mui/icons-material/Clear';
 import { IColumn } from '../../types';
+import ConfirmDialog from '../../components/ConfirmationModal';
 
 export const BoardItem: FC<IColumn> = ({ id, title, order }) => {
   const [titleInput, setTitleInput] = useState(title);
   const [titleEdit, setTitleEdit] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
   const dispatch = useAppDispatch();
   const { boardId } = useParams();
 
@@ -67,39 +69,53 @@ export const BoardItem: FC<IColumn> = ({ id, title, order }) => {
   };
 
   return (
-    <Grid item xs={4}>
-      <Paper
-        sx={{
-          minHeight: 300,
-          minWidth: 200,
-          maxWidth: 220,
-          backgroundColor: '#1A2027',
-          display: 'flex',
-        }}
+    <>
+      <ConfirmDialog
+        title="Delete column?"
+        open={confirmOpen}
+        setOpen={setConfirmOpen}
+        onConfirm={handleDelete}
       >
-        <ClickAwayListener onClickAway={handleClickAway}>
-          <Typography color="#FFFFFF" component="div" sx={{ padding: 0.8, width: 151, height: 40 }}>
-            {!titleEdit ? (
-              <Box onClick={toggleTitle} sx={{ cursor: 'pointer' }} color="#FFFFFF">
-                {title}
-              </Box>
-            ) : (
-              <Paper variant="elevation" sx={{ display: 'flex', height: 30 }}>
-                <IconButton color="primary" size="small" onClick={handeleClickSub}>
-                  <CheckIcon />
-                </IconButton>
-                <IconButton color="primary" size="small" onClick={handleClickCancel}>
-                  <CancelOutlinedIcon />
-                </IconButton>
-                <InputBase color="secondary" placeholder={title} onChange={handleOnChange} />
-              </Paper>
-            )}
-          </Typography>
-        </ClickAwayListener>
-        <IconButton sx={{ height: 40 }} onClick={handleDelete}>
-          <ClearIcon color="primary" height={25} />
-        </IconButton>
-      </Paper>
-    </Grid>
+        <Typography>Are you sure you want to delete this column?</Typography>
+      </ConfirmDialog>
+      <Grid item xs={4}>
+        <Paper
+          sx={{
+            minHeight: 300,
+            minWidth: 200,
+            maxWidth: 220,
+            backgroundColor: '#1A2027',
+            display: 'flex',
+          }}
+        >
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <Typography
+              color="#FFFFFF"
+              component="div"
+              sx={{ padding: 0.8, width: 151, height: 40 }}
+            >
+              {!titleEdit ? (
+                <Box onClick={toggleTitle} sx={{ cursor: 'pointer' }} color="#FFFFFF">
+                  {title}
+                </Box>
+              ) : (
+                <Paper variant="elevation" sx={{ display: 'flex', height: 30 }}>
+                  <IconButton color="primary" size="small" onClick={handeleClickSub}>
+                    <CheckIcon />
+                  </IconButton>
+                  <IconButton color="primary" size="small" onClick={handleClickCancel}>
+                    <CancelOutlinedIcon />
+                  </IconButton>
+                  <InputBase color="secondary" placeholder={title} onChange={handleOnChange} />
+                </Paper>
+              )}
+            </Typography>
+          </ClickAwayListener>
+          <IconButton sx={{ height: 40 }} onClick={() => setConfirmOpen(true)}>
+            <ClearIcon color="primary" height={25} />
+          </IconButton>
+        </Paper>
+      </Grid>
+    </>
   );
 };
