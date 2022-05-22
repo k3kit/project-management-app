869a15e27ce210f5454a0a43538f00da8a222158
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-22 22:51:40 +0300

    feat: display tasks

--- my-app/src/pages/board-page/boardItem.tsx
+++ my-app/src/pages/board-page/boardItem.tsx
@@ -7,22 +7,25 @@ import {
   IconButton,
   InputBase,
   Paper,
+  Stack,
   Typography,
 } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { useAppDispatch } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { deleteColums, updateTitleColumns } from '../../store/slices/columns';
 
 import ClearIcon from '@mui/icons-material/Clear';
 import { IColumn } from '../../types';
 import ConfirmDialog from '../../components/ConfirmationModal';
-import { getTasks } from '../../store/slices/task';
+import { getTasks, ITask } from '../../store/slices/task';
 import { ColumnTitle } from './columnTitle';
+import { TaskItem } from './taskItem';
 
 export const BoardItem: FC<IColumn> = ({ id, title, order }) => {
   const [confirmOpen, setConfirmOpen] = useState(false);
   const dispatch = useAppDispatch();
   const { boardId } = useParams();
+  const { task } = useAppSelector((state) => state.taskReducer);
   useEffect(() => {
     if (boardId) {
       dispatch(getTasks({ IdBoard: boardId, IdColumn: id }));
@@ -52,13 +55,30 @@ export const BoardItem: FC<IColumn> = ({ id, title, order }) => {
             minWidth: 200,
             maxWidth: 220,
             backgroundColor: '#1A2027',
-            display: 'flex',
           }}
         >
-          <ColumnTitle id={id} title={title} order={order}></ColumnTitle>
-          <IconButton sx={{ height: 40 }} onClick={() => setConfirmOpen(true)}>
-            <ClearIcon color="primary" height={25} />
-          </IconButton>
+          <Box sx={{ display: 'flex' }}>
+            <ColumnTitle id={id} title={title} order={order}></ColumnTitle>
+            <IconButton sx={{ height: 40 }} onClick={() => setConfirmOpen(true)}>
+              <ClearIcon color="primary" height={25} />
+            </IconButton>
+          </Box>
+          <Box
+            className="box"
+            sx={{ flexDirection: 'column', overflow: 'auto', maxHeight: '50vh' }}
+          >
+            {task.map((it: ITask) => (
+              <TaskItem
+                key={it.id}
+                id={it.id}
+                title={it.title}
+                order={it.order}
+                done={it.done}
+                description={it.description}
+                userId={it.userId}
+              />
+            ))}
+          </Box>
         </Paper>
       </Grid>
     </>
