e460dd11a52da4a92e200c5722b6dc2e40b355f8
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-23 21:45:32 +0300

    refactor: add slice task

--- my-app/src/store/slices/columns.ts
+++ my-app/src/store/slices/columns.ts
@@ -1,8 +1,10 @@
 import { responsiveFontSizes } from '@mui/material';
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { create } from 'domain';
-import columnsService, { Icolumn } from '../../services/columns.service';
+import { title } from 'process';
+import columnsService, { Icolumn, Task } from '../../services/columns.service';
 import { IColumn, ICreateColumn, IUpdateColumns } from '../../types';
+import { ITask } from './task';
 
 export const createColumns = createAsyncThunk(
   'columns/add',
@@ -41,6 +43,10 @@ interface IcolumnDelete {
   boardId: string;
   columnId: string;
 }
+interface IcolumnDeletee {
+  boardId: string;
+  columnId: string;
+}
 export const deleteColums = createAsyncThunk(
   'columns/delete',
   async ({ boardId, columnId }: IcolumnDelete, ThunkAPI) => {
@@ -52,17 +58,59 @@ export const deleteColums = createAsyncThunk(
     }
   }
 );
+export const getColumnById = createAsyncThunk(
+  'columns/id',
+  async ({ boardId, columnId }: IcolumnDeletee, ThunkAPI) => {
+    try {
+      const respons = await columnsService.getColumnId(boardId, columnId);
+      console.log(respons.data);
+      return respons.data;
+    } catch (error) {
+      ThunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
+interface TaskType {
+  boardId: string;
+  columnId: string;
+  task: Task;
+}
+export const addTask = createAsyncThunk(
+  'task/add',
+  async ({ boardId, columnId, task }: TaskType, ThunkAPI) => {
+    try {
+      const respons = await columnsService.createTask(boardId, columnId, task);
+      console.log(respons.data);
+      return respons.data;
+    } catch (error) {
+      ThunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+// export interface IIIII {
+//   id: string;
+//   title: string;
+//   order: number;
+//   tasks: ITask[];
+// }
 interface ColumnState {
   columns: IColumn[];
+  isLoading: boolean;
+  dialog: boolean;
 }
 
-const initialState: ColumnState = { columns: [] };
+const initialState: ColumnState = { columns: [], isLoading: false, dialog: false };
 
 export const columnsSlice = createSlice({
   name: 'columns',
   initialState,
-  reducers: {},
+  reducers: {
+    setOpen(state: { dialog: boolean }, action: PayloadAction<boolean>) {
+      state.dialog = action.payload;
+    },
+  },
   extraReducers: {
     [createColumns.fulfilled.type]: (state, action) => {
       state.columns.push(action.payload);
@@ -76,6 +124,24 @@ export const columnsSlice = createSlice({
     },
     [deleteColums.fulfilled.type]: (state, action) => {
       state.columns = state.columns.filter((column) => column.id !== action.payload);
+    },
+    [getColumnById.fulfilled.type]: (state, action) => {
+      const index = state.columns.findIndex((columns) => columns.id === action.payload.id);
+      state.columns[index].tasks = action.payload.tasks;
+      state.isLoading = false;
+    },
+    [getColumnById.pending.type]: (state, action) => {
+      state.isLoading = true;
+    },
+    [addTask.fulfilled.type]: (state, action) => {
+      const it = state.columns.find(({ id }) => id === action.payload.columnId);
+      it?.tasks.push({
+        id: action.payload.userId,
+        title: action.payload.title,
+        order: action.payload.order,
+        description: action.payload.description,
+        userId: action.payload.userId,
+      });
     },
   },
 });
