e9c3e61bcea08880e84458d39e19ee1f64f6c3fd
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-25 14:55:12 +0300

    refactor: add dnd for column

--- my-app/src/pages/board-page/boardPage.tsx
+++ my-app/src/pages/board-page/boardPage.tsx
@@ -3,10 +3,11 @@ import { Box, Container, Grid } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { Header } from '../../components/header';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import { getColumns } from '../../store/slices/columns';
+import { getColumnById, getColumns } from '../../store/slices/columns';
 import { IColumn } from '../../types';
 import { BoardItem } from './boardItem';
 import { ButtonCreate } from './buttonCreate';
+import { DragDropContext, Droppable, DroppableProvided, DropResult } from 'react-beautiful-dnd';
 
 const BoardPage = () => {
   const { boardId } = useParams();
@@ -18,25 +19,40 @@ const BoardPage = () => {
       dispatch(getColumns(boardId));
     }
   }, [boardId, dispatch]);
-
+  const handleOnDragEnd = (result: DropResult) => {
+    
+    console.log(result);
+  };
   return (
     <>
       <Header />
       <Box padding={2}>
         <Container>
-          <Grid container spacing={{ xs: 2, height: '85vh' }}>
-            {columns &&
-              columns.map((column: IColumn) => (
-                <BoardItem
-                  key={column.id}
-                  id={column.id}
-                  title={column.title}
-                  order={column.order}
-                  tasks={[]}
-                />
-              ))}
-            <ButtonCreate />
-          </Grid>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="columns" direction="horizontal" type="column">
+              {(provided: DroppableProvided) => (
+                <Grid
+                  container
+                  spacing={{ xs: 2, height: '85vh' }}
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {columns.map((column: IColumn, index: number) => (
+                    <BoardItem
+                      key={column.id}
+                      id={column.id}
+                      title={column.title}
+                      order={column.order}
+                      tasks={[]}
+                      index={index}
+                    />
+                  ))}
+                  <ButtonCreate />
+                  {provided.placeholder}
+                </Grid>
+              )}
+            </Droppable>
+          </DragDropContext>
         </Container>
       </Box>
     </>
