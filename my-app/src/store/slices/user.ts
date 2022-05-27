aa8e40ec507e21b6d2c1657146b0862e02349da0
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-27 16:47:04 +0300

    refactor: refactor initail state

--- my-app/src/store/slices/user.ts
+++ my-app/src/store/slices/user.ts
@@ -1,9 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import authService from '../../services/auth.service';
 import userService from '../../services/user.service';
-interface userData {
-  id: string;
-}
+
 export const getUsers = createAsyncThunk('/users', async (_, thunkAPI) => {
   try {
     const response = await userService.getAllUsers();
@@ -20,8 +18,16 @@ export const deleteUsers = createAsyncThunk('/users', async (id: string, thunkAP
     return thunkAPI.rejectWithValue(error);
   }
 });
-
-const initialState = { users: {}, error: '', user: {}, deleteUser: {} };
+export const getUserById = createAsyncThunk('/users', async (id: string, thunkAPI) => {
+  try {
+    const response = await userService.getUser(id);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+const initialState = { users: {}, error: '', user: '', deleteUser: {} };
 
 export const usersSlice = createSlice({
   name: 'user',
@@ -33,6 +39,9 @@ export const usersSlice = createSlice({
     },
     [getUsers.rejected.type]: (state, action) => {
       state.error = action.payload;
+    },
+    [getUserById.fulfilled.type]: (state, action) => {
+      state.user = action.payload.name;
     },
   },
 });
