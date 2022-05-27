f2acb1d03577b04d1bda5474c180e762a2c0ced6
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-27 16:49:23 +0300

    refactor: updated backend and changed link

--- my-app/src/appConstants/api.ts
+++ my-app/src/appConstants/api.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'https://calm-savannah-72235.herokuapp.com/';
+// export const API_URL = 'https://calm-savannah-72235.herokuapp.com/';
+export const API_URL = 'https://boiling-fortress-08553.herokuapp.com/';
