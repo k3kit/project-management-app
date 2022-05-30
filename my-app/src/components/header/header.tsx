aad6f969953374810bbd174340cfb41501d4ec8d
k3kit <70204612+k3kit@users.noreply.github.com>
2022-05-30 19:37:01 +0300

    refacto: move file

---
+++ my-app/src/components/header/header.tsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from 'react';
+import {
+  AppBar,
+  Button,
+  Stack,
+  Toolbar,
+  Typography,
+  Switch,
+  Container,
+  Hidden,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useAppDispatch } from '../../hooks/redux';
+import { logout } from '../../store/slices/Auth';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { boardsSlice } from '../../store/slices/boards';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+export const Header = () => {
+  const [fix, setFix] = useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const dispath = useAppDispatch();
+  const { setOpen } = boardsSlice.actions;
+  const location = useLocation();
+  const { boardId } = useParams();
+
+  const setFixed = () => {
+    if (window.scrollY >= 100) {
+      setFix(true);
+    } else {
+      setFix(false);
+    }
+  };
+
+  window.addEventListener('scroll', setFixed);
+  const handleLogout = () => {
+    dispath(logout());
+  };
+  useEffect(() => {});
+
+  const openModal = () => {
+    dispath(setOpen(true));
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  return (
+    <AppBar className={fix ? 'appBar fixed' : 'appBar'} position="sticky">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {`${location.pathname}` === `/board/${boardId}` ? (
+                <Button color="info" variant="text" startIcon={<ArrowBackIosIcon />} size="small">
+                  <Link to="/main">Go main</Link>
+                </Button>
+              ) : (
+                ''
+              )}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Button color="inherit">
+                    <Link to="/edit"> Edit profile </Link>
+                  </Button>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Button color="inherit" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Button onClick={openModal} color="inherit">
+                  Create new board
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {`${location.pathname}` === `/board/${boardId}` ? (
+              <Button
+                color="info"
+                variant="contained"
+                startIcon={<ArrowBackIosIcon />}
+                size="small"
+              >
+                <Link to="/main">Go main</Link>
+              </Button>
+            ) : (
+              ''
+            )}
+            <Button sx={{ mr: 2 }} color="inherit">
+              <Link to="/edit"> Edit profile </Link>
+            </Button>
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+            <Button onClick={openModal} color="inherit">
+              Create new board
+            </Button>
+          </Box>
+          <Box>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography>EN</Typography>
+              <Switch defaultChecked color="secondary" />
+              <Typography>RU</Typography>
+            </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
---
+++ my-app/src/components/task/editTask.tsx
@@ -0,0 +1,115 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Container, CssBaseline, Box, Typography, TextField, Button, Grid } from '@mui/material';
+import jwtDecode from 'jwt-decode';
+import { FC, useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { useParams } from 'react-router';
+import { string } from 'yup/lib/locale';
+import { useAppDispatch } from '../../hooks/redux';
+import { logout } from '../../store/slices/Auth';
+import { updateTask } from '../../store/slices/columns';
+import { deleteUsers, updateUser } from '../../store/slices/user';
+import { IFormEdit, Jwt } from '../../types';
+import validationShema from '../../yup';
+import ConfirmDialog from '../modal/ConfirmationModal';
+interface ITaskEdit {
+  title?: string;
+  description?: string;
+}
+
+interface MyProps {
+  titleIntup: string;
+  descriptionInput: string;
+  columnsId: string;
+  id: string;
+  title: string;
+  order: number;
+  description: string;
+}
+const EditTask: FC<MyProps> = ({
+  titleIntup,
+  descriptionInput,
+  columnsId,
+  id,
+  title,
+  order,
+  description,
+}) => {
+  const { boardId } = useParams();
+  const dispath = useAppDispatch();
+  const { control, handleSubmit } = useForm<ITaskEdit>();
+
+  const tokenA = JSON.parse(localStorage.getItem('token') || 'null');
+  const tok = tokenA.token;
+  const decoded = jwtDecode<Jwt>(tok);
+  const onSubmit: SubmitHandler<ITaskEdit> = (data) => {
+    if (boardId && title && description) {
+      dispath(
+        updateTask({
+          boardId: boardId,
+          columnsId: columnsId,
+          taskId: id,
+          task: {
+            title: data.title,
+            order: order,
+            description: data.description,
+            userId: decoded.userId,
+            boardId: boardId,
+            columnId: columnsId,
+          },
+        })
+      );
+    }
+  };
+  return (
+    <Container>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="title"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Title"
+              defaultValue={titleIntup}
+              autoFocus
+              onChange={(e) => field.onChange(e)}
+            />
+          )}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              margin="normal"
+              fullWidth
+              label="description"
+              defaultValue={descriptionInput}
+              autoFocus
+              onChange={(e) => field.onChange(e)}
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          fullWidth={true}
+          disableElevation={true}
+          sx={{
+            marginTop: 1,
+            marginBottom: 1,
+          }}
+        >
+          Save
+        </Button>
+        <Grid container>
+          <Grid item xs></Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default EditTask;
